399ee437bbe9aca02b6d49ffffbb3425328170e1
Om12ek99 <omar.elkamili@gmail.com>
2024-04-13 23:03:39 +0200

    add: variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const divNumberElem = document.querySelector(".number");
+const divSymbolElem = document.querySelector(".symbol");
+let divDisplayElem = document.getElementById("display");
+let valore1;
+let valore2;
+let cont = 0;
+let operator = "";
 
 
 
