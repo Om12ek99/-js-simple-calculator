86df65b31aac00eb2c22a0546fe576a9fcdc7628
Om12ek99 <omar.elkamili@gmail.com>
2024-04-13 23:04:10 +0200

    add:create number

--- js/script.js
+++ js/script.js
@@ -6,5 +6,10 @@ let valore2;
 let cont = 0;
 let operator = "";
 
+for (i = 1; i <= 11; i++) {
+  const newNumber = createNumber(i);
+  console.log(newNumber);
+  divNumberElem.append(newNumber);
+}
 
 
