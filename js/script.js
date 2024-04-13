39fa04d89f9b1b22367e317b81e333ef5b933eef
Om12ek99 <omar.elkamili@gmail.com>
2024-04-13 23:04:42 +0200

    add:create symbol

--- js/script.js
+++ js/script.js
@@ -6,10 +6,17 @@ let valore2;
 let cont = 0;
 let operator = "";
 
+// number
 for (i = 1; i <= 11; i++) {
   const newNumber = createNumber(i);
   console.log(newNumber);
   divNumberElem.append(newNumber);
 }
 
+// symbol
+for (i = 1; i <= 5; i++) {
+  const newSymbol = createSymbol(i);
+  console.log(newSymbol);
+  divSymbolElem.append(newSymbol);
+}
 
